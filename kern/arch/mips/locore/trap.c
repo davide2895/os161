3613e9134774d1eb44233c2831d57e23d548136c
davide2895 <b.abbi@hotmail.it>
2023-03-16 20:07:35 +0100

    Crash handling

--- kern/arch/mips/locore/trap.c
+++ kern/arch/mips/locore/trap.c
@@ -112,9 +112,12 @@ kill_curthread(vaddr_t epc, unsigned code, vaddr_t vaddr)
 	 * You will probably want to change this.
 	 */
 
+	// kprintf("Fatal user mode trap %u sig %d (%s, epc 0x%x, vaddr 0x%x)\n",
+	// 	code, sig, trapcodenames[code], epc, vaddr);
+	// panic("I don't know how to handle this\n");
 	kprintf("Fatal user mode trap %u sig %d (%s, epc 0x%x, vaddr 0x%x)\n",
-		code, sig, trapcodenames[code], epc, vaddr);
-	panic("I don't know how to handle this\n");
+	code, sig, trapcodenames[code], epc, vaddr);
+	sys__exit(code); //If there is one of the above errors, we call the sys__exit() and exit both current thread and current process
 }
 
 /*
